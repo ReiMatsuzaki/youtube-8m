c65d6a311482f67b323e9bf04308004353fcce35
Rei Matsuzaki <matsuzaki.rei@gmail.com>
2018-07-14 20:26:12 +0900

    try/err for 16infe

--- 16infe_fmoe.sh
+++ 16infe_fmoe.sh
@@ -1,6 +1,13 @@
 BUCKET_NAME=gs://reim2zk_us
 JOB_NAME=yt8m_16infe_fmoe_$(date +%Y%m%d_%H%M%S)
 TRAIN_DIR=$BUCKET_NAME/yt8m_16_fmoe
+
+# copy model file and
+#mkdir -p out_16_fmoe/model
+#gsutil cp gs://reim2zk_us/yt8m_16_fmoe/inference_model* out_16_fmoe/model
+#gsutil cp gs://reim2zk_us/yt8m_16_fmoe/model_flags.json out_16_fmoe/model/
+#tar -zcvf out_16_fmoe/model.tgz out_16_fmoe/model
+
 gcloud --verbosity=debug ml-engine jobs \
 submit training $JOB_NAME \
 --runtime-version 1.4 \
@@ -9,8 +16,9 @@ submit training $JOB_NAME \
 --config=youtube-8m/cloudml-gpu.yaml \
 -- --input_data_pattern='gs://youtube8m-ml-us-east1/2/frame/test/test*.tfrecord' \
 --frame_features=True \
---train_dir=$TRAIN_DIR \
---output_file=$TRAIN_DIR/16_fmoe.csv \
---output_model_tgz=$TRAIN_DIR/16_fmoe_model.tgz
+--train_dir=$TRAIN_DIR \       
+--input_model_tgz="16_fmoe/model.tgz" \
+--output_file=$TRAIN_DIR/16_fmoe.csv
+
 
 
--- 16localinfe_fmoe.sh
+++ 16localinfe_fmoe.sh
@@ -1,13 +1,17 @@
-BUCKET_NAME=gs://reim2zk_us
-JOB_NAME=yt8m_16infe_fmoe_$(date +%Y%m%d_%H%M%S)
-TRAIN_DIR=$BUCKET_NAME/yt8m_16_fmoe
-gcloud --verbosity=debug ml-engine \
-local train \
+#BUCKET_NAME=gs://reim2zk_us
+
+# copy model file and
+#mkdir -p out_16_fmoe/model
+#gsutil cp gs://reim2zk_us/yt8m_16_fmoe/inference_model* out_16_fmoe/model
+#gsutil cp gs://reim2zk_us/yt8m_16_fmoe/model_flags.json out_16_fmoe/model/
+#tar -zcvf out_16_fmoe/model.tgz out_16_fmoe/model
+
+gcloud --verbosity=debug ml-engine local train \
 --package-path=youtube-8m --module-name=youtube-8m.inference \
--- --input_data_pattern='gs://youtube8m-ml-us-east1/2/frame/test/test*.tfrecord' \
+-- --input_data_pattern='gs://youtube8m-ml-us-east1/2/frame/test/test0001.tfrecord' \
 --frame_features=True \
---train_dir=$TRAIN_DIR \
---output_file=output_16_fmoe.csv \
---output_model_tgz=output_16_fmoe_model.tgz
+--input_model_tgz="out_16_fmoe/model.tgz" \
+--output_file="out_16_fmoe/16_fmoe.csv"
+
 
 
