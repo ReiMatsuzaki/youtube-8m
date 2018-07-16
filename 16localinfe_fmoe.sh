af31db41f1afba9c52777ef85565eb1c0b78304c
Rei Matsuzaki <matsuzaki.rei@gmail.com>
2018-07-16 23:59:31 +0900

    modify infe

--- 16localinfe_fmoe.sh
+++ 16localinfe_fmoe.sh
@@ -1,17 +1,21 @@
-#BUCKET_NAME=gs://reim2zk_us
+#gcloud --verbosity=debug ml-engine local train \
+#--package-path=youtube-8m --module-name=youtube-8m.inference \
+#-- --input_data_pattern='gs://youtube8m-ml-us-east1/2/frame/test/test0001.tfrecord' \
+#--frame_features=True \
+#--input_model_tgz="out_16_fmoe/model_16_fmoe.tgz" \
+#--output_file="out_16_fmoe/16_fmoe.csv"
 
-# copy model file and
-#mkdir -p out_16_fmoe/model
-#gsutil cp gs://reim2zk_us/yt8m_16_fmoe/inference_model* out_16_fmoe/model
-#gsutil cp gs://reim2zk_us/yt8m_16_fmoe/model_flags.json out_16_fmoe/model/
-#tar -zcvf out_16_fmoe/model.tgz out_16_fmoe/model
-
-gcloud --verbosity=debug ml-engine local train \
---package-path=youtube-8m --module-name=youtube-8m.inference \
--- --input_data_pattern='gs://youtube8m-ml-us-east1/2/frame/test/test0001.tfrecord' \
---frame_features=True \
---input_model_tgz="out_16_fmoe/model.tgz" \
---output_file="out_16_fmoe/16_fmoe.csv"
-
+NAME=16_fmoe
+YT8M=youtube-8m
+OUT_DIR=out_$NAME
+INPUT_DIR=frame
+LOGFILE=${OUT_DIR}/infe.log
+LANG=C; date > $LOGFILE
+python ${YT8M}/inference.py \
+       --input_data_pattern=${INPUT_DIR}/test0000.tfrecord \
+       --input_model_tgz $OUT_DIR/model_$NAME.tgz \
+       --output_file=${OUT_DIR}/solution_$NAME.csv
+       >> $LOGFILE 2>&1
+LANG=C; date >> $LOGFILE
 
 
