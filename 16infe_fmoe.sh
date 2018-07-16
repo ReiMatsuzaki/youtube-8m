83de5bf2de83c95b61f9c07117a2e05487da4f9b
Rei Matsuzaki <matsuzaki.rei@gmail.com>
2018-07-17 00:55:44 +0900

    16infe

--- 16infe_fmoe.sh
+++ 16infe_fmoe.sh
@@ -10,15 +10,16 @@ TRAIN_DIR=$BUCKET_NAME/yt8m_16_fmoe
 
 gcloud --verbosity=debug ml-engine jobs \
 submit training $JOB_NAME \
---runtime-version 1.4 \
+--runtime-version 1.8 \
+--python-version=3.5 \
 --package-path=youtube-8m --module-name=youtube-8m.inference \
 --staging-bucket=$BUCKET_NAME --region=us-east1 \
 --config=youtube-8m/cloudml-gpu.yaml \
 -- --input_data_pattern='gs://youtube8m-ml-us-east1/2/frame/test/test*.tfrecord' \
 --frame_features=True \
 --train_dir=$TRAIN_DIR \       
---input_model_tgz="16_fmoe/model.tgz" \
---output_file=$TRAIN_DIR/16_fmoe.csv
+--output_model_tgz=$TRAIN_DIR/model_16_fmoe.tgz \
+--output_file=$TRAIN_DIR/submission_16_fmoe.csv
 
 
 
--- 16localmodel_fmoe.sh
+++ 16localmodel_fmoe.sh
@@ -9,6 +9,9 @@ MODEL_NAME=model_16_fmoe
 mkdir -p $OUT_DIR/$MODEL_NAME
 gsutil cp $TRAIN_DIR/inference_model* $OUT_DIR/$MODEL_NAME
 gsutil cp $TRAIN_DIR/model_flags.json $OUT_DIR/$MODEL_NAME
-cd $OUT_DIR; tar zcvf $MODEL_NAME.tgz $MODEL_NAME/
+cd $OUT_DIR/$MODEL_NAME; tar zcvf $MODEL_NAME.tgz *
+mv $MODEL_NAME.tgz ../
+
+
 
 
