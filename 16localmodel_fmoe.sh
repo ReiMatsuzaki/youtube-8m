5d357e42cc4dc45995b5b71d49715800449f424a
Rei Matsuzaki <matsuzaki.rei@gmail.com>
2018-07-16 23:11:03 +0900

    modify localmodel for 16 and 17

--- 16localmodel_fmoe.sh
+++ 16localmodel_fmoe.sh
@@ -1,13 +1,15 @@
 # copy model file and archive it
 
 BUCKET_NAME=gs://reim2zk_us
-TRAIN_DIR=$BUCKET_NAME/yt8m_17_fcg2moe
+TRAIN_DIR=$BUCKET_NAME/yt8m_16_fmoe
 
-out_dir=out_17_fcg2moe
-model_dir=${out_dir}/17_model
+out_dir=out_16_fmoe
+model_dir=${out_dir}/16_model
 model_tgz=${model_dir}.tgz
 
 mkdir -p $model_dir
 gsutil cp $TRAIN_DIR/inference_model* $model_dir
 gsutil cp $TRAIN_DIR/model_flags.json $model_dir/
 tar -zcvf $model_tgz $model_dir
+
+
