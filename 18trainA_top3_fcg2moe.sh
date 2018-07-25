5800bba2c1701724dfcf010d1d9f9dfc7dfa6737
Matsuzaki Rei <matsuzaki.rei@gmail.com>
2018-07-25 17:32:01 +0900

    failed on google ml

--- 18trainA_top3_fcg2moe.sh
+++ 18trainA_top3_fcg2moe.sh
@@ -1,13 +1,13 @@
-BUCKET_NAME=gs://reim2zk_us
+BUCKET_NAME=gs://reim2zk_us2
 JOB_NAME=yt8m_18trainA_top3_fcg2moe_$(date +%Y%m%d_%H%M%S)
 TRAIN_DIR=$BUCKET_NAME/yt8m_18_top3_fcg2moe
 gcloud --verbosity=debug ml-engine jobs \
 submit training $JOB_NAME \
 --runtime-version 1.4 \
 --package-path=youtube-8m --module-name=youtube-8m.train \
---staging-bucket=$BUCKET_NAME --region=us-east1 \
+--staging-bucket=$BUCKET_NAME --region=us-central1 \
 --config=youtube-8m/cloudml-gpu.yaml \
--- --train_data_pattern='gs://youtube8m-ml-us-east1/2/frame/train/train*.tfrecord' \
+-- --train_data_pattern='gs://youtube8m-ml-us-central1/2/frame/train/train*.tfrecord' \
 --frame_features=True \
 --model=MeanStdTopkVideoModel \
 --video_level_classifier=MoeModel \
